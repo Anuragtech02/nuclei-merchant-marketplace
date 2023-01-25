cdfb49ca2d17293cacdce8c9f8b92664b6f9c778
Anurag Pal <apal895@gmail.com>
2023-01-25 09:35:08 +0530

    fight details done

--- src/components/FlightDetails.svelte
+++ src/components/FlightDetails.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	// subscribe to GlobalStore
+	const FLIGHT_DETAILSL_HEIGHT = 80;
 	import { GlobalStore } from '../utils/globalStore';
 	import { getCityByCode } from '../utils/functions';
 	const { subscribe, update } = GlobalStore;
@@ -11,36 +11,34 @@
 	});
 </script>
 
-<section class="w-[80%] mx-auto">
-	<div class="p-2 flex justify-between items-center">
-		<div class="flex-col items-center">
-			<div>
-				<img src="icons/dot.svg" alt="dot" />
-			</div>
-			<div class="w-[2px] bg-black h-[5px]" />
-			<div>
-				<img src="icons/plane.svg" alt="airplane" />
-			</div>
-		</div>
-		<div class="flex-1">
-			<div class="flex justify-start">
-				<span class="border-solid border-2 px-2 py-1 border-radius rounded-md">
-					{startLocation}
-				</span>
-				<p class="ml-2">{getCityByCode(startLocation)}</p>
-			</div>
-			<hr />
-			<div class="flex justify-start">
-				<span class="border-solid border-2 px-2 py-1 border-radius rounded-md">
-					{endLocation}
-				</span>
-				<p class="ml-2">{getCityByCode(endLocation)}</p>
-			</div>
-		</div>
+<div class="px-2 py-6 flex justify-between items-center gap-2 bg-white px-5 rounded-lg text-black">
+	<div class={`flex flex-col items-center justify-between h-[${FLIGHT_DETAILSL_HEIGHT}px] gap-1`}>
 		<div>
-			<button>
-				<img src="icons/swap.svg" alt="swap" />
-			</button>
+			<img src="icons/dot.svg" alt="dot" />
+		</div>
+		<div class="w-[1px] bg-black flex-1" />
+		<div>
+			<img src="icons/plane.svg" alt="airplane" />
 		</div>
 	</div>
-</section>
+	<div class={`flex-1 h-[${FLIGHT_DETAILSL_HEIGHT}px] flex flex-col justify-between`}>
+		<div class="flex justify-start">
+			<span class="border-solid border-2 px-2 py-1 border-radius rounded-md text-[9px]">
+				{startLocation}
+			</span>
+			<p class="ml-2">{getCityByCode(startLocation)}</p>
+		</div>
+		<hr class="border-dashed border-[#606574] my-2" />
+		<div class="flex justify-start">
+			<span class="border-solid border-2 px-2 py-1 border-radius rounded-md text-[9px]">
+				{endLocation}
+			</span>
+			<p class="ml-2">{getCityByCode(endLocation)}</p>
+		</div>
+	</div>
+	<div>
+		<button class="btn bg-transparent border-none p-0 py-0 hover:bg-transparent">
+			<img src="icons/swap.svg" alt="swap" />
+		</button>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,18 +5,18 @@
 </script>
 
 <Appbar title="Flights" backLink="/">
-	<div slot="extras" class="min-w-[35%] flex justify-between items-center">
+	<div slot="extras" class="min-w-[35%] sm:min-w-[70%] flex justify-between items-center">
 		<button class="btn py-[4px] px-[16px] bg-[#1BA4F7] text-white">
 			<img src="/icons/wallet.svg" alt="wallet" /> &nbsp; ₹ {balance}
 		</button>
 		<button class="btn bg-[#1BA4F7] text-white">
 			<img src="/icons/percentage.svg" alt="offers" />
 		</button>
-		<button class="btn bg-transparent border-none">
+		<button class="btn bg-transparent border-none p-0 ml-2">
 			<img src="/icons/kebab-menu.svg" alt="menu" />
 		</button>
 	</div>
 </Appbar>
-<section>
+<section class="w-[80%] mx-auto mt-6">
 	<FlightDetails />
 </section>
